3d3338cf97042c2d7560952b2afe8aef317b299e
hayato94087 <hayato94087@gmail.com>
2024-04-25 05:41:13 +0900

    feat:存在しないURLを指定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ const Page: FC<Props> = (props) => {
               <div className="flex flex-col space-y-3 bg-blue-100 p-5">
                 <div className="flex flex-row">
                   <h2 className="text-sm text-blue-800 py-2 px-3 rounded-md underline underline-offset-2 hover:text-blue-600">
-                    <Link href={"/user/12345"}>
+                    <Link href={"/hoge/12345"}>
                       http://localhost:3000/user/12345
                     </Link>
                   </h2>
